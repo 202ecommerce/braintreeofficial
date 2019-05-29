d6cb28dc27d20cb78ee70f05fffcab95c28df357
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2019-05-29 13:13:07 +0300

    refs #18534 added module

---
+++ braintree.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * 2007-2019 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2019 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+require_once(_PS_MODULE_DIR_.'braintree/vendor/autoload.php');
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+use BraintreePPBTlib\Module\PaymentModule;
+use BraintreePPBTlib\Extensions\ProcessLogger\ProcessLoggerExtension;
+
+
+class Braintree extends PaymentModule
+{
+    /**
+     * List of hooks used in this Module
+     */
+    public $hooks = array(
+        'paymentOptions',
+        'displayOrderConfirmation',
+        'displayAdminOrder',
+        'actionOrderStatusPostUpdate',
+        'actionOrderStatusUpdate',
+        'header',
+        'actionObjectCurrencyAddAfter',
+        'displayBackOfficeHeader',
+        'actionBeforeCartUpdateQty',
+        'displayInvoiceLegalFreeText',
+        'actionAdminControllerSetMedia',
+        'displayMyAccountBlock',
+        'displayCustomerAccount',
+        'actionOrderSlipAdd',
+        'displayAdminOrderTabOrder',
+        'displayAdminOrderContentOrder',
+        'displayAdminCartsView'
+    );
+
+    /**
+     * List of ppbtlib extentions
+     */
+    public $extensions = array(
+        BraintreePPBTlib\Extensions\ProcessLogger\ProcessLoggerExtension::class,
+    );
+
+    /**
+     * List of objectModel used in this Module
+     * @var array
+     */
+    public $objectModels = array();
+
+    /**
+     * List of admin tabs used in this Module
+     */
+    public $moduleAdminControllers = array(
+        array(
+            'name' => array(
+                'en' => 'Braintree Official',
+                'fr' => 'Braintree Officiel'
+            ),
+            'class_name' => 'AdminParentBraintreeConfiguration',
+            'parent_class_name' => 'SELL',
+            'visible' => true,
+            'icon' => 'payment'
+        )
+    );
+
+    public function __construct()
+    {
+        $this->name = 'braintree';
+        $this->tab = 'payments_gateways';
+        $this->version = '@version@';
+        $this->author = 'PrestaShop';
+        $this->display = 'view';
+        $this->module_key = '336225a5988ad434b782f2d868d7bfcd';
+        $this->is_eu_compatible = 1;
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->controllers = array('payment', 'validation');
+        $this->bootstrap = true;
+
+        $this->currencies = true;
+        $this->currencies_mode = 'radio';
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Braintree');
+        $this->description = $this->l('Benefit from Braintree’s complete payments platform and grow your business online, on mobile and internationally. Accept credit cards, debit cards and PayPal payments.');
+        $this->confirmUninstall = $this->l('Are you sure you want to delete your details?');
+        $this->module_link = $this->context->link->getAdminLink('AdminModules', true).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+
+        $this->errors = '';
+    }
+
+    public function install()
+    {
+        // Install default
+        if (!parent::install()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function uninstall()
+    {
+        // Uninstall default
+        if (!parent::uninstall()) {
+            return false;
+        }
+        return true;
+    }
+
+    public function hookActionAdminControllerSetMedia()
+    {
+
+    }
+
+    public function hookActionBeforeCartUpdateQty($params)
+    {
+
+    }
+
+    public function hookActionObjectCurrencyAddAfter($params)
+    {
+
+    }
+
+    public function hookActionOrderSlipAdd($params)
+    {
+
+    }
+    public function hookActionOrderStatusPostUpdate(&$params)
+    {
+
+    }
+
+    public function hookActionOrderStatusUpdate(&$params)
+    {
+
+    }
+
+    public function hookDisplayAdminCartsView($params)
+    {
+
+    }
+
+    public function hookDisplayAdminOrder($params)
+    {
+
+    }
+
+    public function hookDisplayBackOfficeHeader()
+    {
+
+    }
+
+    public function hookDisplayCustomerAccount()
+    {
+
+    }
+
+    public function hookDisplayInvoiceLegalFreeText($params)
+    {
+
+    }
+
+    public function hookDisplayMyAccountBlock()
+    {
+
+    }
+
+    public function hookDisplayOrderConfirmation($params)
+    {
+
+    }
+
+    public function hookHeader()
+    {
+
+    }
+
+    public function hookPaymentOptions($params)
+    {
+
+    }
+
+}
---
+++ vendor/ppbtlib/src/Extensions/ProcessLogger/Classes/ProcessLoggerObjectModel.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to a commercial license from SARL 202 ecommerce
+ * Use, copy, modification or distribution of this source file without written
+ * license agreement from the SARL 202 ecommerce is strictly forbidden.
+ * In order to obtain a license, please contact us: tech@202-ecommerce.com
+ * ...........................................................................
+ * INFORMATION SUR LA LICENCE D'UTILISATION
+ *
+ * L'utilisation de ce fichier source est soumise a une licence commerciale
+ * concedee par la societe 202 ecommerce
+ * Toute utilisation, reproduction, modification ou distribution du present
+ * fichier source sans contrat de licence ecrit de la part de la SARL 202 ecommerce est
+ * expressement interdite.
+ * Pour obtenir une licence, veuillez contacter 202-ecommerce <tech@202-ecommerce.com>
+ * ...........................................................................
+ *
+ * @author    202-ecommerce <tech@202-ecommerce.com>
+ * @copyright Copyright (c) 202-ecommerce
+ * @license   Commercial license
+ * @version   develop
+ */
+
+namespace BraintreePPBTlib\Extensions\ProcessLogger\Classes;
+
+use \ObjectModel;
+
+class ProcessLoggerObjectModel extends ObjectModel
+{
+    /** @var string log */
+    public $log;
+
+    /* @var int id_order*/
+    public $id_order;
+
+    /* @var int id_cart*/
+    public $id_cart;
+
+    /* @var int id_shop*/
+    public $id_shop;
+
+    /* @var string id_transaction*/
+    public $id_transaction;
+
+    /* @var string status*/
+    public $status;
+
+    /* @var bool sandbox*/
+    public $sandbox;
+
+    /* @var string tools*/
+    public $tools;
+
+    /* @var string creation date*/
+    public $date_add;
+
+    /* @var string date of transaction*/
+    public $date_transaction;
+
+    /**
+     * @see \ObjectModel::$definition
+     */
+    public static $definition = array(
+        'table'        => 'braintree_processlogger',
+        'primary'      => 'id_braintree_processlogger',
+        'fields'       => array(
+            'id_order'     => array(
+                'type'     => ObjectModel::TYPE_INT,
+                'validate' => 'isUnsignedId',
+                'size'     => 11,
+            ),
+            'id_cart'     => array(
+                'type'     => ObjectModel::TYPE_INT,
+                'validate' => 'isUnsignedId',
+                'size'     => 11,
+            ),
+            'id_shop'     => array(
+                'type'     => ObjectModel::TYPE_INT,
+                'validate' => 'isUnsignedId',
+                'size'     => 11,
+            ),
+            'id_transaction'     => array(
+                'type'     => ObjectModel::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'size'     => 50,
+            ),
+            'log'     => array(
+                'type'     => ObjectModel::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'size'     => 1000,
+            ),
+            'status'     => array(
+                'type'     => ObjectModel::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'size'     => 20,
+            ),
+            'sandbox'     => array(
+                'type'     => ObjectModel::TYPE_BOOL,
+                'validate' => 'isBool'
+            ),
+            'tools'     => array(
+                'type'     => ObjectModel::TYPE_STRING,
+                'validate' => 'isGenericName',
+                'size'     => 50,
+            ),
+            'date_add'     => array(
+                'type'     => ObjectModel::TYPE_DATE,
+                'validate' => 'isDate',
+            ),
+            'date_transaction'     => array(
+                'type'     => ObjectModel::TYPE_DATE,
+                'validate' => 'isDate',
+            ),
+        ),
+    );
+
+    public function getLinkToTransaction()
+    {
+        throw new \Exception('Need to define the method ' . __FUNCTION__);
+    }
+}
