be11e884a19e7157d2a5713e7717128aa0953e46
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2019-10-03 16:28:09 +0300

    refs #20498 fix multishop issue

--- braintreeofficial.php
+++ braintreeofficial.php
@@ -339,7 +339,13 @@ class BraintreeOfficial extends \PaymentModule
     {
         foreach (Currency::getCurrencies() as $currency) {
             $maName = Tools::strtoupper($this->getNameMerchantAccountForCurrency($currency['iso_code'], $mode));
-            Configuration::deleteByName($maName);
+            $where = 'name="' . pSQL($maName) .'"';
+
+            if (Shop::isFeatureActive()) {
+                $where .= ' AND id_shop=' . $this->context->shop->id;
+            }
+
+            Db::getInstance()->delete('configuration', $where);
         }
     }
 
