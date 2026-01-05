97552921f7091d05968d8f64ebfc40179587e65f
bogdan202 <byampolskiy@202-ecommerce.com>
2026-01-05 12:23:23 +0100

    refs #55417 js error slideToggle is not a function

--- _dev/js/utils.js
+++ _dev/js/utils.js
@@ -25,5 +25,5 @@
 
 $(document).on('click', '[data-role-collapse]', function (e) {
     let collapsedElId = $(e.target).attr('data-collapsed');
-    $.find(collapsedElId).slideToggle();
+    $(collapsedElId).slideToggle();
 });
